a06b928254bc286af8078fe0524521e79f24940f
BykaWF <yaroslavprozorov213@gmail.com>
2023-08-01 11:13:27 +0200

    Added GET method

--- src/main/java/com/yaroslavcode/Main.java
+++ src/main/java/com/yaroslavcode/Main.java
@@ -1,24 +1,31 @@
 package com.yaroslavcode;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Objects;
 
 
 @SpringBootApplication // this gives us all needed configuration in one line
 
 @RestController
+@RequestMapping("api/v1/customer")
 public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
+    }
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
     }
 
 }
