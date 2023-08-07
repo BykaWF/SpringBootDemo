ca1890d5f4e79b32d85b3083064b963dd24cc5d1
BykaWF <yaroslavprozorov213@gmail.com>
2023-08-07 19:17:29 +0200

    Renamed annotation

--- src/main/java/com/yaroslavcode/customer/CustomerFakeRepo.java
+++ src/main/java/com/yaroslavcode/customer/CustomerFakeRepo.java
@@ -1,10 +1,11 @@
 package com.yaroslavcode.customer;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
 import java.util.List;
-@Component(value = "fake")
+@Repository(value = "fake")
 public class CustomerFakeRepo implements  CustomerRepo{
     @Override
     public List<Customer> getCustomers() {
--- src/main/java/com/yaroslavcode/customer/CustomerRepository.java
+++ src/main/java/com/yaroslavcode/customer/CustomerRepository.java
@@ -1,13 +1,12 @@
 package com.yaroslavcode.customer;
 //DAO LAYER
 import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
-import java.util.Collection;
+
 import java.util.Collections;
 import java.util.List;
-@Component
+@Repository
 @Primary
 public class CustomerRepository implements CustomerRepo
 {
