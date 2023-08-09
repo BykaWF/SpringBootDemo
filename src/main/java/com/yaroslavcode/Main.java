7d8df966128e750faf22681339d6698b94dbb074
BykaWF <yaroslavprozorov213@gmail.com>
2023-08-09 16:33:46 +0200

    Added FeignClient

--- src/main/java/com/yaroslavcode/Main.java
+++ src/main/java/com/yaroslavcode/Main.java
@@ -3,6 +3,7 @@ package com.yaroslavcode;
 import com.yaroslavcode.customer.CustomerController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -11,7 +12,8 @@ import java.util.List;
 
 
 
-@SpringBootApplication // this gives us all needed configuration in one line
+@SpringBootApplication// this gives us all needed configuration in one line
+@EnableFeignClients
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
