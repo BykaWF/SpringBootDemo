837e6cd759234b0d38b4a5ffac38970f544b08d5
BykaWF <yaroslavprozorov213@gmail.com>
2023-08-10 14:58:11 +0200

    Added Unit test

---
+++ src/test/java/com/yaroslavcode/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.yaroslavcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest // for integration test
+public class DemoApplicationTests {
+    @Test
+    void contextLoads(){
+
+    }
+}
