92e097c3defe559e104c189a686210e2957e08be
BykaWF <yaroslavprozorov213@gmail.com>
2023-08-10 11:04:18 +0200

    Added dev environment spring profile

--- src/main/java/com/yaroslavcode/customer/CustomerConfiguration.java
+++ src/main/java/com/yaroslavcode/customer/CustomerConfiguration.java
@@ -1,19 +1,29 @@
 package com.yaroslavcode.customer;
 
+import com.yaroslavcode.infoapp.InfoApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomerConfiguration {
     @Value("${app.useFakeCustomerRepo: false}")
     private Boolean useFakeCustomerRepo;
-
+    @Value("${info.app.name}")
+    private String companyName;
+    @Autowired
+    private Environment environment;
     @Bean
-    CommandLineRunner commandLineRunner() {
+    CommandLineRunner commandLineRunner(InfoApp infoApp) {
         return args -> {
             System.out.println("Command line runner");
+            System.out.println(companyName);
+            System.out.println(
+                    environment.getProperty("info.company.name"));
+            System.out.println(infoApp);
         };
     }
 
